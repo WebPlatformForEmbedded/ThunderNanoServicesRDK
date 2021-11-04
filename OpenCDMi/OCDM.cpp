5bcd9004b5efa7aec86e3fa5b4b647237ff5addf
brainstem123 <c.custers@metrological.com>
2021-11-04 15:10:35 +0100

    [development/METROL369]: Suppress unused variable warning in non-debug build.

--- OpenCDMi/OCDM.cpp
+++ OpenCDMi/OCDM.cpp
@@ -140,27 +140,24 @@ namespace Plugin {
 
         _opencdmi->Deinitialize(service);
 
-        if (_connectionId != 0) {
+        RPC::IRemoteConnection* connection(_service->RemoteConnection(_connectionId));
 
-            RPC::IRemoteConnection* connection(_service->RemoteConnection(_connectionId));
+        VARIABLE_IS_NOT_USED uint32_t result = _opencdmi->Release();
 
-            uint32_t result = _opencdmi->Release();
+        // It should have been the last reference we are releasing,
+        // so it should end up in a DESCRUCTION_SUCCEEDED, if not we
+        // are leaking...
+        ASSERT(result == Core::ERROR_DESCRUCTION_SUCCEEDED);
 
-            // It should have been the last reference we are releasing,
-            // so it should end up in a DESCRUCTION_SUCCEEDED, if not we
-            // are leaking...
-            ASSERT(result == Core::ERROR_DESCRUCTION_SUCCEEDED);
+        // If this was running in a (container) proccess...
+        if (connection != nullptr) {
 
-            // If this was running in a (container) proccess...
-            if (connection != nullptr) {
-
-                // Lets trigger the cleanup sequence for
-                // out-of-process code. Which will guard
-                // that unwilling processes, get shot if
-                // not stopped friendly :~)
-                connection->Terminate();
-                connection->Release();
-            }
+            // Lets trigger the cleanup sequence for
+            // out-of-process code. Which will guard
+            // that unwilling processes, get shot if
+            // not stopped friendly :~)
+            connection->Terminate();
+            connection->Release();
         }
 
         PluginHost::ISubSystem* subSystem = service->SubSystems();
