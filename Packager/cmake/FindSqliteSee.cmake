d82a400bb5a734990af6ef849226a535c49f1136
Haseena Sainul <h.sainul@metrological.com>
2022-03-04 03:12:34 -0800

    Packager: syncup from rdkservices

--- Packager/cmake/FindSqliteSee.cmake
+++
@@ -1,8 +0,0 @@
-find_package(PkgConfig)
-
-if(SqliteSee_FIND_QUIETLY)
-    set(_SQLITE_SEE_MODE QUIET)
-elseif(SqliteSee_FIND_REQUIRED)
-    set(_SQLITE_SEE_MODE REQUIRED)
-endif()
-pkg_search_module(SQLITE_SEE ${_SQLITE_SEE_MODE} sqlite3see)
