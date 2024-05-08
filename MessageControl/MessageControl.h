6081b907b4e55728845b38a4073109f3bbb9ef4b
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2024-05-08 11:55:30 +0200

    [Messaging] Adding a method to get all available modules, Controls method now also takes a module name (#298)
    * The initial commit after which oop only controls are yet missed
    
    * Changing the modules list to a vector to match framework changes

--- MessageControl/MessageControl.h
+++ MessageControl/MessageControl.h
@@ -407,18 +407,30 @@ namespace Plugin {
             }
         }
 
-        uint32_t Enable(const messagetype type, const string& category, const string& module, const bool enabled) override
+        Core::hresult Enable(const messagetype type, const string& category, const string& module, const bool enabled) override
         {
             _client.Enable({static_cast<Core::Messaging::Metadata::type>(type), category, module}, enabled);
 
             return (Core::ERROR_NONE);
         }
 
-        uint32_t Controls(Exchange::IMessageControl::IControlIterator*& controls) const override
+        Core::hresult Modules(Exchange::IMessageControl::IStringIterator*& modules) const override
+        {
+            std::vector<string> list;
+
+            _client.Modules(list);
+
+            using Implementation = RPC::IteratorType<RPC::IStringIterator>;
+            modules = Core::ServiceType<Implementation>::Create<RPC::IStringIterator>(list);
+
+            return (Core::ERROR_NONE);
+        }
+
+        Core::hresult Controls(const string& module, Exchange::IMessageControl::IControlIterator*& controls) const override
         {
             std::list<Exchange::IMessageControl::Control> list;
             Messaging::MessageUnit::Iterator index;
-            _client.Controls(index);
+            _client.Controls(index, module);
 
             while (index.Next() == true) {
                 list.push_back( { static_cast<messagetype>(index.Type()), index.Category(), index.Module(), index.Enabled() } );
