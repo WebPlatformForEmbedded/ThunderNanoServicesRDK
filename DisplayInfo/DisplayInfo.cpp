2d7961b31e97d731007b49ac5b621429ec4c446b
sebaszm <s.szczepaniak@metrological.com>
2025-02-07 12:37:55 +0100

    [DisplayInfo] Remove RESTful API support

--- DisplayInfo/DisplayInfo.cpp
+++ DisplayInfo/DisplayInfo.cpp
@@ -36,10 +36,6 @@ namespace Plugin {
         );
     }
 
-
-    static Core::ProxyPoolType<Web::Response> responseFactory(4);
-    static Core::ProxyPoolType<Web::JSONBodyType<JsonData::DisplayInfo::DisplayinfoData>> jsonResponseFactory(4);
-
     /* virtual */ const string DisplayInfo::Initialize(PluginHost::IShell* service)
     {
         string message;
@@ -156,81 +152,6 @@ namespace Plugin {
     {
         // No additional info to report.
         return (string());
-    }
-
-    /* virtual */ void DisplayInfo::Inbound(Web::Request& /* request */)
-    {
-    }
-
-    /* virtual */ Core::ProxyType<Web::Response> DisplayInfo::Process(const Web::Request& request)
-    {
-        ASSERT(_skipURL <= request.Path.length());
-
-        Core::ProxyType<Web::Response> result(PluginHost::IFactories::Instance().Response());
-
-        // By default, we assume everything works..
-        result->ErrorCode = Web::STATUS_OK;
-        result->Message = "OK";
-
-        // <GET> - currently, only the GET command is supported, returning system info
-        if (request.Verb == Web::Request::HTTP_GET) {
-
-            Core::ProxyType<Web::JSONBodyType<JsonData::DisplayInfo::DisplayinfoData>> response(jsonResponseFactory.Element());
-
-            Core::TextSegmentIterator index(Core::TextFragment(request.Path, _skipURL, static_cast<uint32_t>(request.Path.length()) - _skipURL), false, '/');
-
-            // Always skip the first one, it is an empty part because we start with a '/' if there are more parameters.
-            index.Next();
-
-            Info(*response);
-            result->ContentType = Web::MIMETypes::MIME_JSON;
-            result->Body(Core::ProxyType<Web::IBody>(response));
-        } else {
-            result->ErrorCode = Web::STATUS_BAD_REQUEST;
-            result->Message = _T("Unsupported request for the [DisplayInfo] service.");
-        }
-
-        return result;
-    }
-
-    void DisplayInfo::Info(JsonData::DisplayInfo::DisplayinfoData& displayInfo) const
-    {
-        uint64_t ram = 0;
-        if (_graphicsProperties->TotalGpuRam(ram) == Core::ERROR_NONE) {
-            displayInfo.Totalgpuram = ram;
-        }
-        ram = 0;
-        if (_graphicsProperties->FreeGpuRam(ram) == Core::ERROR_NONE) {
-            displayInfo.Freegpuram = ram;
-        }
-
-        bool status = false;
-        if (_connectionProperties->IsAudioPassthrough(status) == Core::ERROR_NONE) {
-            displayInfo.Audiopassthrough = status;
-        }
-        status = false;
-        if (_connectionProperties->Connected(status) == Core::ERROR_NONE) {
-            displayInfo.Connected = status;
-        }
-
-        uint32_t value = 0;
-        if (_connectionProperties->Width(value) == Core::ERROR_NONE) {
-            displayInfo.Width = value;
-        }
-        value = 0;
-        if (_connectionProperties->Height(value) == Core::ERROR_NONE) {
-            displayInfo.Height = value;
-        }
-
-        Exchange::IConnectionProperties::HDCPProtectionType hdcpProtection(Exchange::IConnectionProperties::HDCPProtectionType::HDCP_Unencrypted);
-        if ((const_cast<const Exchange::IConnectionProperties*>(_connectionProperties))->HDCPProtection(hdcpProtection) == Core::ERROR_NONE) {
-            displayInfo.Hdcpprotection = static_cast<JsonData::DisplayInfo::DisplayinfoData::HdcpprotectionType>(hdcpProtection);
-        }
-
-        Exchange::IHDRProperties::HDRType hdrType(Exchange::IHDRProperties::HDRType::HDR_OFF);
-        if (_hdrProperties->HDRSetting(hdrType) == Core::ERROR_NONE) {
-            displayInfo.Hdrtype = static_cast<JsonData::DisplayInfo::DisplayinfoData::HdrtypeType>(hdrType);
-        }
     }
 
     void DisplayInfo::Deactivated(RPC::IRemoteConnection* connection)
