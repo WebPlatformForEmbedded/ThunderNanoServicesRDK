58f2846fc8252e66065a28a16bad8f0f23b7f085
VeithMetro <m.daniluk@metrological.com>
2025-02-07 12:05:40 +0100

    Remove unnecessary includes of JsonData files

--- WebKitBrowser/WebKitBrowser.h
+++ WebKitBrowser/WebKitBrowser.h
@@ -25,8 +25,6 @@
 #include <interfaces/IApplication.h>
 #include <interfaces/IMemory.h>
 
-#include <interfaces/json/JsonData_Browser.h>
-#include <interfaces/json/JsonData_WebKitBrowser.h>
 #include <interfaces/json/JsonData_StateControl.h>
 #include <interfaces/json/JWebBrowser.h>
 #include <interfaces/json/JWebBrowserExt.h>
