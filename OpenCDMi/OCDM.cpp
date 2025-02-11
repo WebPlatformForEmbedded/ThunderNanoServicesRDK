e78307815f16f726473a50a36e15b2c291b03160
Mateusz Daniluk <121170681+VeithMetro@users.noreply.github.com>
2025-02-11 10:38:56 +0100

    [IOpenCDM] Update after changes to the interface (#339)
    * Adjust the plugin after making changes to the interface
    
    * Adjust after renaming to OpenCDM
    
    * Don't forget to update the md as well..
    
    * Update after changing the error code on the interface
    
    * Update the md
    
    * Correct the include typo

--- OpenCDMi/OCDM.cpp
+++ OpenCDMi/OCDM.cpp
@@ -124,6 +124,7 @@ namespace Plugin {
                 message = _T("OCDM could not be instantiated.");
             } else {
                 RegisterAll();
+                Exchange::JOpenCDM::Register(*this, this);
                 _opencdmi->Initialize(_service);
 
                 ASSERT(_connectionId != 0);
@@ -161,6 +162,7 @@ namespace Plugin {
 
                 _opencdmi->Deinitialize(service);
 
+                Exchange::JOpenCDM::Unregister(*this);
                 UnregisterAll();
 
                 RPC::IRemoteConnection* connection(_service->RemoteConnection(_connectionId));
@@ -285,6 +287,24 @@ namespace Plugin {
                 PluginHost::IShell::DEACTIVATED,
                 PluginHost::IShell::FAILURE));
         }
+    }
+
+    Core::hresult OCDM::Systems(Exchange::IOpenCDM::IStringIterator*& keySystems) const
+    {
+        ASSERT(_opencdmi != nullptr);
+
+        keySystems = _opencdmi->Systems();
+
+        return (Core::ERROR_NONE);
+    }
+
+    Core::hresult OCDM::Designators(const string& keySystem, Exchange::IOpenCDM::IStringIterator*& designators) const
+    {
+        ASSERT(_opencdmi != nullptr);
+
+        designators = _opencdmi->Designators(keySystem);
+
+        return (designators == nullptr ? Core::ERROR_UNKNOWN_KEY : Core::ERROR_NONE);
     }
 }
 } //namespace Thunder::Plugin
--- OpenCDMi/OCDM.h
+++ OpenCDMi/OCDM.h
@@ -24,11 +24,13 @@
 #include <interfaces/IContentDecryption.h>
 #include <interfaces/IMemory.h>
 #include <interfaces/json/JsonData_OCDM.h>
+#include <interfaces/IOCDM.h>
+#include <interfaces/json/JOpenCDM.h>
 
 namespace Thunder {
 namespace Plugin {
 
-    class OCDM : public PluginHost::IPlugin, public PluginHost::IWeb, public PluginHost::JSONRPC {
+    class OCDM : public Exchange::IOpenCDM, public PluginHost::IPlugin, public PluginHost::IWeb, public PluginHost::JSONRPC {
     private:
 
         class Notification : public RPC::IRemoteConnection::INotification {
@@ -158,6 +160,7 @@ POP_WARNING()
         INTERFACE_ENTRY(PluginHost::IDispatcher)
         INTERFACE_AGGREGATE(Exchange::IContentDecryption, _opencdmi)
         INTERFACE_AGGREGATE(Exchange::IMemory, _memory)
+        INTERFACE_ENTRY(Exchange::IOpenCDM)
         END_INTERFACE_MAP
 
     public:
@@ -197,6 +200,9 @@ POP_WARNING()
         void UnregisterAll();
         uint32_t get_drms(Core::JSON::ArrayType<JsonData::OCDM::DrmData>& response) const;
         uint32_t get_keysystems(const string& index, Core::JSON::ArrayType<Core::JSON::String>& response) const;
+
+        Core::hresult Systems(Exchange::IOpenCDM::IStringIterator*& keySystems) const override;
+        Core::hresult Designators(const string& keySystem, Exchange::IOpenCDM::IStringIterator*& designators) const override;
 
     private:
         uint8_t _skipURL;
