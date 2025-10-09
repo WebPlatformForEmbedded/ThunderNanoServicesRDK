4fe077172ee08253be5b8c26329000a9cf1ff16b
sebaszm <45654185+sebaszm@users.noreply.github.com>
2025-10-09 10:08:37 +0200

    Add BridgeLink plugin (#365)
    * Add BridgeLink plugin
    
    * Make compile

---
+++ BridgeLink/Module.h
@@ -0,0 +1,14 @@
+#ifndef __MODULE_BRIDGELINK_MODULE_H
+#define __MODULE_BRIDGELINK_MODULE_H
+
+#ifndef MODULE_NAME
+#define MODULE_NAME Plugin_BridgeLink
+#endif
+
+#include <plugins/plugins.h>
+#include <definitions/definitions.h>
+
+#undef EXTERNAL
+#define EXTERNAL
+
+#endif // __MODULE_BRIDGELINK_MODULE_H
